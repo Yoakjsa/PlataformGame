34c2d716c82c8e67fc217389a22d9921ea3d7f10
Yoaksa <yoaksa.flores@gmail.com>
2022-10-24 04:07:32 -0500

    door
    Add openDoor

--- Videojuego3D/Assets/Scripts/DialogDoor.cs
+++ Videojuego3D/Assets/Scripts/DialogDoor.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialog : MonoBehaviour
+public class DialogDoor : MonoBehaviour
 {
        [SerializeField] TMP_Text dialogs;
        [SerializeField] GameObject dialogPanel;
        [SerializeField] GameManager gameManager;
+       [SerializeField] Animator openDoorAnim;
+
+       bool doorOpen;
 
      private void OnTriggerStay(Collider other)
      {
           if(other.CompareTag("Player"))
           {
                dialogPanel.SetActive(true);
-               if (gameManager.haveKey==false)
+               if (gameManager.haveKey==false&&!doorOpen)
                {
-                    dialogs.text="!Necesitas encontrar una llave!";
+                    dialogs.text="¡Necesitas encontrar una llave!";
                }
-               else
+               else 
                {
                     dialogs.text="Presiona 'k' para abrir la puerta";
                }
           }
+
+          if(Input.GetKey(KeyCode.K))
+          {
+               openDoorAnim.SetBool("UseKey", true);
+               doorOpen=true;
+          }
+
+          if(doorOpen)
+          {
+          dialogs.text="¡Felicidades!, avanza al siguiente nivel";
+          }
+     
      }
 
      private void OnTriggerExit(Collider other)
